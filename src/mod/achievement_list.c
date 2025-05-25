662ead5e788945faef8953e7d957bd4007d61326
ThatHypedPerson <thathypedperson@gmail.com>
2025-05-25 01:30:03 -0700

    Define many basic achievements.

--- src/mod/achievement_list.c
+++ src/mod/achievement_list.c
@@ -2,10 +2,11 @@
 
 
 Achievement gAchievements[] = { 
+    // Item Screen
     {
-        "test_achievement",
-        "Test Achievement",
-        "An achievement for testing.",
+        "get_ocarina",
+        "Recover the Ocarina of Time",
+        "Grab the Ocarina after knocking it out of Skull Kid's hands atop Clock Tower.",
         NULL,
         NULL,
         NULL,
@@ -13,7 +14,894 @@ Achievement gAchievements[] = {
             0
         },
         NULL
-    }
+    },
+    {
+        "get_bow",
+        "Get the Hero's Bow",
+        "Obtain the Hero's Bow from Woodfall Temple.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_bow_upgrade_40",
+        "Win the Large Quiver",
+        "Score at least 40 points in the Town Shooting Gallery, or 2120 points in the Swamp Shooting Gallery.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_bow_upgrade_50",
+        "Win the Largest Quiver",
+        "Score at least 40 points in the Town Shooting Gallery and 2120 points in the Swamp Shooting Gallery.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_fire_arrow",
+        "Get the Fire Arrow",
+        "Obtain the Fire Arrow by defeating the Wizzrobe guarding it in Snowhead Temple.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_ice_arrow",
+        "Get the Ice Arrow",
+        "Obtain the Ice Arrow by defeating Wart in Great Bay Temple.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_light_arrow",
+        "Get the Light Arrow",
+        "Obtain the Light Arrow by defeating the Garo Master in Stone Tower Temple.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_bomb_bag",
+        "Buy the Bomb Bag",
+        "Purchase the Bomb Bag from the Bomb Shop in West Clock Town.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_bomb_bag_upgrade_30",
+        "Buy the Big Bomb Bag",
+        "After stopping the thief on Night 1, purchase the Big Bomb Bag from the Bomb Shop in West Clock Town.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_bomb_bag_upgrade_40",
+        "Buy the Biggest Bomb Bag",
+        "Purchase the Biggest Bomb Bag from the Business Scrub in the Goron Village.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_bombchus",
+        "Obtain Bombchus",
+        "After obtaining a Bomb Bag, buy some from the Bomb Shop or find them across Termina.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_sticks",
+        "Obtain Deku Sticks",
+        "Obtain some Deku Sticks throughout your adventure.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_nuts",
+        "Obtain Deku Nuts",
+        "Obtain some Deku Nuts throughout your adventure.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_magic_beans",
+        "Get Magic Beans",
+        "Learn how to use Magic Beans and get a free sample from the Bean Seller in a grotto in Deku Palace.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_powder_keg",
+        "Pass the Powder Keg Test",
+        "Get approval from Medigoron in Goron Village to use Powder Kegs by passing his test.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_pictobox",
+        "Get the Pictograph Box",
+        "Obtain the Pictograph Box before taking the Boat Cruise from the Swamp Tourist Center.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_lens_of_truth",
+        "Get the Lens of Truth",
+        "Follow the owl, Kaepora Garbora, from Goron Village to the Lone Peak Shrine to obtain the Lens of Truth.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_hookshot",
+        "Get the Hookshot",
+        "Sneak through the Pirates' Fortress and obtain the Hookshot.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_great_fairy_sword",
+        "Get the Great Fairy Sword",
+        "Get rewarded with the Great Fairy Sword after returning all the Stray Fairies from Stone Tower Temple to the Fairy's Fountain in Ikana Canyon.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    // Bottles
+    {
+        "get_bottle_swamp",
+        "A Witch's Medicine",
+        "After finding Koume collapsed in the Woods of Mystery, talk to her sister Kotake to get a Red Potion to heal her.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_bottle_gold_dust",
+        "Goron Race Winner",
+        "After clearing Snowhead Temple, win the Goron Race behind the boulder in Twin Islands.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_bottle_romani",
+        "Ranch Defender",
+        "On the First Night, help Romani by preventing the aliens from abducting the cows in Romani Ranch.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_bottle_dampe",
+        "Grave Digger",
+        "On the Final Night, enter the grave guarded by the Stalchildren and attempt to find treasure with Dampé.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_bottle_beavers",
+        "Better than the Beavers",
+        "Climb up the trees in Zora Cape with your Hookshot and beat the Beaver Brothers' challenges to obtain their bottle.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_bottle_chateau",
+        "Tipsy Mailman",
+        "Deliver the Special Delivery to Mama yourself to Madame Aroma in the Milk Bar.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    // Masks
+    {
+        "get_mask_deku",
+        "Get the Deku Mask",
+        "Trick the Happy Mask Salesman into healing you.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_goron",
+        "Get the Goron Mask",
+        "Follow the Goron ghost with the Lens of Truth back to Mountain Village, climb the invisible ladders, and recieve the Goron Mask.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_zora",
+        "Get the Zora Mask",
+        "Attempt to heal the soul of the Zora washed ashore in Great Bay Coast.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_fierce_diety",
+        "",
+        "Give all your masks away to the Moon Children in order to play on even grounds.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    // TODO: Quest Items
+    // Non-Transformation Masks (approximately ordered in obtainability)
+    {
+        "get_mask_great_fairy",
+        "Get the Great Fairy's Mask",
+        "Return the Stray Fairy in Clock Town as a Human.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_kafei",
+        "Get Kafei's Mask",
+        "Talk to Madame Aroma in the Mayor's Residence in East Clock Town to start the search for Kafei.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_bremen",
+        "Get the Bremen Mask",
+        "Listen to Guru-Guru's story and obtain the Bremen Mask.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_kamaro",
+        "Get Kamaro's Mask",
+        "Heal Kamaro atop the platform in the snowy region of Termina Field.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_blast",
+        "Get the Blast Mask",
+        "Stop the thief in North Clock Town at Midnight on the First Night.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_truth",
+        "Get the Mask of Truth",
+        "Collect all 30 Gold Skulltula Spirits in the Swamp Spider House to break the man's curse.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_bunny_hood",
+        "Get the Bunny Hood",
+        "Lead all the baby Cuccos in Romani Ranch's Cucco Shack using the Bremen Mask to make Grog happy.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_keaton",
+        "Get the Keaton Mask",
+        "Grab what Kafei left behind in the Laundry Pool as he chased Sakon on Day 3.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_postman",
+        "Get the Postman's Hat",
+        "Let the Postman deliver the Special Delivery to Mama and talk to him before he flees.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_scents",
+        "Get the Mask of Scents",
+        "Follow the Deku Butler through Deku Shrine after returning the Deku Princess.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_don_gero",
+        "Get Don Gero's Mask",
+        "Knock a Rock Sirloin out of a pot in the Goron Shrine and take it to the hungry Goron in Mountain Village.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_romani",
+        "Get Romani's Mask",
+        "Help Cremia transport Romani Milk to Clock Town.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_garo",
+        "Get the Garo's Mask",
+        "Win the race against the Gorman Brothers.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_captain",
+        "Get the Captain's Hat",
+        "Awaken Captain Keeta in the Ikana Graveyard and complete his challenge.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_stone",
+        "Get the Stone Mask",
+        "Help the invisible soldier Shiro regain his strength by giving him some medicine.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_circus_leader",
+        "Get the Circus Leader's Mask",
+        "Help with the sound check in the Milk Bar during the first or second night to inspire someone feeling down.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_all_night",
+        "Get the All-Night Mask",
+        "After preventing the thief in North Clock Town on Night 1, purchase the item in the Curiosity Shop on Night 3.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_gibdo",
+        "Get the Gibdo Mask",
+        "Heal the cursed man inside the Music Box House atop Ikana Canyon.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_couple",
+        "Get the Couple's Mask",
+        "Reunite Anju and Kafei and watch over as they fulfil their promise with each other.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_mask_giant",
+        "Get the Giant's Mask",
+        "Defeat the Eyegore in Inverted Stone Tower Temple.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    // Remains
+    {
+        "get_remains_odolwa",
+        "Obtain Odolwa's Remains",
+        "Defeat Odolwa in Woodfall Temple.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_remains_goht",
+        "Obtain Goht's Remains",
+        "Defeat Goht in Snowhead Temple.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_remains_gyorg",
+        "Obtain Gyorg's Remains",
+        "Defeat Gyorg in Great Bay Temple.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_remains_twinmold",
+        "Obtain Twinmold's Remains",
+        "Defeat Twinmold in Stone Tower Temple.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    // Songs
+    // Song of Time skipped (get with Ocarina)
+    // Song of Healing skipped (get with Deku Mask)
+    {
+        "play_song_inverted_sot",
+        "Invert Time",
+        "Play the Inverted Song of Time.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "play_song_double_sot",
+        "Skip Time",
+        "Play the Song of Double Time.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_song_epona",
+        "Learn Epona's Song",
+        "Practice against Them with Romani on the First Day in Romani Ranch.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_song_soaring",
+        "Learn the Song of Soaring",
+        "Meet with Kaepora Gaebora atop the flowers in Southern Swamp.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_song_storms",
+        "Learn the Song of Storms",
+        "Defeat the Iron Knuckle guarding Flat's grave in Ikana Graveyard.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_song_sonata",
+        "Learn the Sonata of Awakening",
+        "Meet with the falsely imprisoned Monkey in Deku Palace.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_song_lullaby_intro",
+        "Learn the Goron Lullaby (Intro)",
+        "Unfreeze the Goron Elder and tell him about his son.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_song_lullaby",
+        "Learn the Goron Lullaby",
+        "Attempt to soothe the Goron Child with what you've been taught about the Goron Lullaby.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_song_new_wave",
+        "Learn the New Wave Bossa Nova",
+        "Return all Zora Eggs to the Marine Research Lab.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_song_elegy",
+        "Learn the Elegy of Emptiness",
+        "Defeat Igos du Ikana within Ikana Castle.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_song_oath",
+        "Learn the Oath to Order",
+        "Learn the song the Giants try to teach you.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_song_scarecrow",
+        "Scarecrow's Song",
+        "Play a nice tune for a scarecrow.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    // Quest Screen
+    // Kokiri Sword + Hero Shield skipped (get with Deku Mask)
+    {
+        "get_sword_razor",
+        "Get the Razor Sword",
+        "Get your Kokiri Sword upgraded at the Mountain Smithy in Mountain Village.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_sword_gilded",
+        "Get the Gilded Sword",
+        "Strengthen your Razor Sword with Gold Dust at the Mountain Smithy.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_shield_mirror",
+        "Get the Mirror Shield",
+        "Traverse through all of Beneath the Well to receive your prize.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "get_bombers_notebook",
+        "Get the Bombers' Notebook",
+        "Find all the Bombers in their game of Hide and Seek.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    // Challenges
+    {   // missable
+        "break_razor_sword",
+        "100 Swings",
+        "Use the Razor Sword until it breaks.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "find_all_scarecrows",
+        "Scarecrow Search",
+        "Find every scarecrow across Termina.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "complete_bombers_notebook",
+        "Bomber's Networking",
+        "Completely fill out your Bomber's Notebook.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {
+        "activate_all_owl_statues",
+        "Quick Travel",
+        "Activate all Owl Statues across Termina.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
+    {   // missable
+        "giants_oath_failure",
+        "Not Enough Remains",
+        "Attempt to play Oath to Order atop Clock Tower with only Three Remains.",
+        NULL,
+        NULL,
+        NULL,
+        {
+            0
+        },
+        NULL
+    },
 };
 
 u32 GetAchievementCount() {
